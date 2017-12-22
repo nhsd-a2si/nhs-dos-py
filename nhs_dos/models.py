ce6d9c074c99dece05cd8f405d501db68af3dbcb
Matt Stibbs <git@stibbsy.co.uk>
2017-12-22 23:06:34 +0000

    Move Endpoint model into endpoints.py

--- nhs_dos/models.py
+++ nhs_dos/models.py
@@ -1,3 +1,6 @@
+from nhs_dos.endpoints import Endpoint
+
+
 class Service:
     def __init__(self, service_json):
         self.json = service_json
@@ -6,20 +9,6 @@ class Service:
         self.endpoints = []
         for ep in service_json['endpoints']:
             self.endpoints.append(Endpoint(ep))
-
-
-class Endpoint:
-    def __init__(self, endpoint):
-        parts = endpoint['value'].split('\|')
-        self.transport = endpoint['tag']
-        self.order = endpoint['order']
-        self.uri = parts[0]
-        self.interaction = parts[1]
-        self.format = parts[2]
-        self.scenario = parts[3]
-        self.message = parts[4]
-        self.compressed = parts[5]
-
 
 class ServiceList:
     def __init__(self, service_list=None):
